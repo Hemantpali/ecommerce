94a645c45ccdba326aa3539eca63f094e1cd2272
Paliwal <hemantpali200@gmail.com>
2026-06-28 22:42:58 +0530

    fix width

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,7 @@ if (nodeEnv === 'development') {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 300,
   standardHeaders: true,
   legacyHeaders: false,
   message: { success: false, message: 'Too many requests, please try again later' },
